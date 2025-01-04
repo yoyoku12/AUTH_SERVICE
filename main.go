66e5a7cdb7343cee13641ae61291e5342b0df383
demLuna <dlxyork@icloud.com>
2025-01-04 11:51:41 +0400

    fixed

--- main.go
+++ main.go
@@ -11,44 +11,35 @@ import (
 )
 
 func main() {
-	// Загружаем переменные окружения
+
 	env.LoadEnv()
 
-	// Подключаемся к базе данных
 	dbConn := db.Connect()
 	defer dbConn.Close()
 
-	// Запускаем миграцию
 	db.MigrateToDB(dbConn)
 
-	// Инициализация очистки сессий
 	sessions.InitSessionCleanup()
 
-	// Маршрут для страницы входа
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/login.html")
 	})
 
-	// Маршрут для страницы регистрации
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/register.html")
 	})
 
-	// Маршрут для динамического отображения профиля
 	http.HandleFunc("/profile", sessions.SessionMiddleware(handlers.ProfileHandler(dbConn)))
 
-	// Маршрут для страницы выхода
 	http.HandleFunc("/logout", sessions.SessionMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/logout.html")
 	}))
 
-	// Регистрация обработчиков для запросов
-	http.HandleFunc("/login_action", handlers.LoginHandler(dbConn))       // Обработчик логина
-	http.HandleFunc("/register_action", handlers.RegisterHandler(dbConn)) // Обработчик регистрации
+	http.HandleFunc("/login_action", handlers.LoginHandler(dbConn))
+	http.HandleFunc("/register_action", handlers.RegisterHandler(dbConn))
 	http.HandleFunc("/profile_action", sessions.SessionMiddleware(handlers.ProfileHandler(dbConn)))
 	http.HandleFunc("/logout_action", sessions.SessionMiddleware(handlers.LogoutHandler()))
 
-	// Запуск сервера
 	log.Println("Сервер запущен на порту 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
